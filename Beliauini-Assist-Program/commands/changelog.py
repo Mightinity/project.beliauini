5ba5275ece41b03a614ca420738a44811ca74d40
Mightinity <maamoo1312@gmail.com>
2023-05-23 13:45:56 +0700

    Fix Minor Typo: Have empty list

--- Beliauini-Assist-Program/commands/changelog.py
+++ Beliauini-Assist-Program/commands/changelog.py
@@ -17,12 +17,13 @@ class Changelog(commands.Cog):
         embed = discord.Embed(
             title="**Change Log**",
             description="""**April 3rd 2023** (v0.1)\n- Beliauini Assist created\n- Command b!ctfschedule created\n- Command /visi-misi created
-            \n**April 4th 2023** (v0.2)\n- \n- Change Command b!ctfschedule --> /ctf schedule\n- Integrated to server\n- Create a presence\n- Command /ctf join created
+            \n**April 4th 2023** (v0.2)\n- Change Command b!ctfschedule --> /ctf schedule\n- Integrated to server\n- Create a presence\n- Command /ctf join created
             \n**April 5th 2023** (v0.3)\n- Command /server-info created\n- Command /version created
             \n**April 9th 2023** (v0.4)\n- Logging system
             \n**April 12th 2023** (v0.5)\n- Command /ctf finish created\n- Fix minor from Logging system
             \n**April 28th 2023** (v0.6)\n- Add countdown on Embed & Join
-            \n**Mei 20th 2023** (v0.7)\n- Implement OOP on bot program\n- Remove /ctf finish command""",
+            \n**Mei 20th 2023** (v0.7)\n- Implement OOP on bot program\n- Remove /ctf finish command,
+            \n**Mei 23th 2023** (v0.8)\n- New Feature: Export Text-Channel System""",
             color=0x2b2c31)
         embed.set_author(name="Beliauini Assist", icon_url=os.getenv("LOGO"))
         embed.set_thumbnail(url=os.getenv("LOGO"))
